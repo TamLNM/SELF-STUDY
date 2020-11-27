9b54564d5078d786741e1aaf4b86b8fb483c07ea
Le Nguyen Minh Tam <tam@andco-web.jp>
2020-11-27 14:07:21 +0700

    [27.11.2020] Update
    All my current source code + images

---
+++ II. CODE - USEFUL EXTENSION/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Emmet</title>
+</head>
+<body>
+    <!-- Create a div with class -->
+    <!-- div.___class1 -->
+    <div class="___class1"></div>
+    
+    <!-- Create a div with class and id -->
+    <!-- div.__class1#div1 -->
+    <div class="___class1" id="div1"></div>
+
+    <!-- Create a div with multiple class -->
+    <!-- div.___class1.___class2 -->
+    <div class="___class1 ___class2"></div>
+
+    <!-- Create the example text -->
+    <!-- lorem20 -->
+    <div>
+        <p class="class1"><a href="#">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet ipsa debitis voluptas dolores inventore voluptates officia odio nulla, autem nostrum.</a></p>
+    </div>
+    
+    <!-- Create multiple same element -->
+    <!-- div.class1*3 -->
+    <div class="class1"></div>
+    <div class="class1"></div>
+    <div class="class1"></div>
+
+</body>
+</html>
+
+
